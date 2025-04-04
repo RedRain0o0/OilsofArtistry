66021ce3f1d871d9ae1c4fb090537b5bb5250588
RedRain0o0 <96890698+RedRain0o0@users.noreply.github.com>
2025-04-03 21:57:30 -0400

    new items

--- src/main/java/io/github/redrain0o0/artistry/item/ArtistryItems.java
+++ src/main/java/io/github/redrain0o0/artistry/item/ArtistryItems.java
@@ -25,21 +25,25 @@ public class ArtistryItems {
     public static final Item AMETHYST_CHISEL;
     //public static final Item AMETHYST_SHIELD;
     public static final Item KNAWED_SWORD;
-    public static final Item SMALL_PINCHER;
+    public static final Item OXIWIND_BOLT;
+    public static final Item SCULK_LATCH;
+    public static final Item SMALL_PINCER;
     public static final Item SPLINTER_SHIELD;
     public static final Item SPLINTERS;
     public static final Item TERMITE_HELMET;
-    public static final Item WOVEN_PINCHER;
+    public static final Item WOVEN_PINCER;
 
     static {
         AMETHYST_CHISEL = register("amethyst_chisel", Item::new, new Item.Properties().durability(432).repairable(ArtistryItemTags.AMETHYST));
         //AMETHYST_SHIELD = register("amethyst_shield", ShieldItem::new, new Item.Properties().durability(336).repairable(ArtistryItemTags.AMETHYST).equippableUnswappable(EquipmentSlot.OFFHAND).component(DataComponents.BLOCKS_ATTACKS, new BlocksAttacks(0.25F, 1.0F,List.of(new BlocksAttacks.DamageReduction(90.0F, Optional.empty(), 0.0F, 1.0F)), new BlocksAttacks.ItemDamageFunction(3.0F, 1.0F, 1.0F), Optional.of(DamageTypeTags.BYPASSES_SHIELD), Optional.of(SoundEvents.SHIELD_BLOCK), Optional.of(SoundEvents.SHIELD_BREAK))).component(DataComponents.BREAK_SOUND, SoundEvents.SHIELD_BREAK));
         KNAWED_SWORD = register("knawed_sword", Item::new, new Item.Properties().sword(ToolMaterial.IRON, 3.0F, -2.4F));
-        SMALL_PINCHER = register("small_pincher", Item::new, new Item.Properties());
-        SPLINTER_SHIELD = register("splinter_shield", SplinterShieldItem::new, new Item.Properties().durability(336).repairable(ItemTags.WOODEN_TOOL_MATERIALS).equippableUnswappable(EquipmentSlot.OFFHAND).component(DataComponents.BLOCKS_ATTACKS, new BlocksAttacks(0.25F, 1.0F,List.of(new BlocksAttacks.DamageReduction(90.0F, Optional.empty(), 0.0F, 1.0F)), new BlocksAttacks.ItemDamageFunction(3.0F, 1.0F, 1.0F), Optional.of(DamageTypeTags.BYPASSES_SHIELD), Optional.of(SoundEvents.SHIELD_BLOCK), Optional.of(SoundEvents.SHIELD_BREAK))).component(DataComponents.BREAK_SOUND, SoundEvents.SHIELD_BREAK));
+        OXIWIND_BOLT = register("oxiwind_bolt", Item::new, new Item.Properties());
+        SCULK_LATCH = register("sculk_latch", Item::new, new Item.Properties());
+        SMALL_PINCER = register("small_pincer", Item::new, new Item.Properties());
+        SPLINTER_SHIELD = register("splinter_shield", SplinterShieldItem::new, new Item.Properties().component(ArtistryComponents.SPLINTER_COUNT, 0).durability(336).repairable(ItemTags.WOODEN_TOOL_MATERIALS).equippableUnswappable(EquipmentSlot.OFFHAND).component(DataComponents.BLOCKS_ATTACKS, new BlocksAttacks(0.25F, 1.0F,List.of(new BlocksAttacks.DamageReduction(90.0F, Optional.empty(), 0.0F, 1.0F)), new BlocksAttacks.ItemDamageFunction(3.0F, 1.0F, 1.0F), Optional.of(DamageTypeTags.BYPASSES_SHIELD), Optional.of(SoundEvents.SHIELD_BLOCK), Optional.of(SoundEvents.SHIELD_BREAK))).component(DataComponents.BREAK_SOUND, SoundEvents.SHIELD_BREAK));
         SPLINTERS = register("splinters", SplintersItem::new, new Item.Properties());
         TERMITE_HELMET = register("termite_helmet", Item::new, new Item.Properties().humanoidArmor(ArtistryArmorMaterials.TERMITE, ArmorType.HELMET));
-        WOVEN_PINCHER = register("woven_pincher", Item::new, new Item.Properties());
+        WOVEN_PINCER = register("woven_pincer", Item::new, new Item.Properties());
     }
 
     public static Item register(String name, Function<Item.Properties, Item> itemFactory, Item.Properties properties) {
@@ -54,11 +58,13 @@ public class ArtistryItems {
         addItemToGroup(ArtistryItems.AMETHYST_CHISEL, Items.BRUSH, CreativeModeTabs.TOOLS_AND_UTILITIES);
         //addItemToGroup(ArtistryItems.AMETHYST_SHIELD, Items.SHIELD, CreativeModeTabs.COMBAT);
         addItemToGroup(ArtistryItems.KNAWED_SWORD, Items.NETHERITE_SWORD, CreativeModeTabs.COMBAT);
-        addItemToGroup(ArtistryItems.SMALL_PINCHER, Items.RABBIT_HIDE, CreativeModeTabs.INGREDIENTS);
+        addItemToGroup(ArtistryItems.OXIWIND_BOLT, Items.HEAVY_CORE, CreativeModeTabs.INGREDIENTS);
+        addItemToGroup(ArtistryItems.SCULK_LATCH, Items.ECHO_SHARD, CreativeModeTabs.INGREDIENTS);
+        addItemToGroup(ArtistryItems.SMALL_PINCER, Items.RABBIT_HIDE, CreativeModeTabs.INGREDIENTS);
         addItemToGroup(ArtistryItems.SPLINTER_SHIELD, Items.SHIELD, CreativeModeTabs.COMBAT);
         addItemToGroup(ArtistryItems.SPLINTERS, Items.ARROW, CreativeModeTabs.COMBAT);
         addItemToGroup(ArtistryItems.TERMITE_HELMET, Items.TURTLE_HELMET, CreativeModeTabs.COMBAT);
-        addItemToGroup(ArtistryItems.WOVEN_PINCHER, ArtistryItems.SMALL_PINCHER, CreativeModeTabs.INGREDIENTS);
+        addItemToGroup(ArtistryItems.WOVEN_PINCER, ArtistryItems.SMALL_PINCER, CreativeModeTabs.INGREDIENTS);
     }
 
     private static void addItemToGroup(Item item, Item itemAfter, ResourceKey<CreativeModeTab> group) {

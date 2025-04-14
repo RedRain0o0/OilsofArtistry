1e57579b205f73a30b2cf17284bc58429fa7f9ef
RedRain0o0 <96890698+RedRain0o0@users.noreply.github.com>
2025-04-14 19:30:44 -0400

    whole buncha new items

---
+++ src/main/java/io/github/redrain0o0/artistry/item/SculkBundleItem.java
@@ -0,0 +1,9 @@
+package io.github.redrain0o0.artistry.item;
+
+import net.minecraft.world.item.BundleItem;
+
+public class SculkBundleItem extends BundleItem {
+    public SculkBundleItem(Properties properties) {
+        super(properties);
+    }
+}
